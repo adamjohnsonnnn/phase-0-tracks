bbe726a1387bd61c601caab4b6c0f4aaec17515a
Adam Johnson <adam.johnson22@gmail.com>
2017-07-24 08:14:44 -0500

    add five vampire detection questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,21 @@
+# Werewolf, Inc. 
+#Checking Name
+puts "What is your name?"
+name = gets.chomp
+
+# Check age
+puts "How old are you?"
+age = gets.chomp
+
+#Check what year applicant was born
+puts = "What year were you born?"
+birth_year = gets.chomp
+
+#Ask about garlic bread
+puts = "Would you like some garlic bread?"
+garlic_bread = gets.chomp
+
+#Ask if they want health insurance
+puts = "Would you like health insurance?"
+health_insurance = gets.chomp
+
