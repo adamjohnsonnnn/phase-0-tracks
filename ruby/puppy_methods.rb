f5ab79db1ee9145ec2378500b36608cba7b2dc66
Adam Johnson <adam.johnson22@gmail.com>
2017-08-04 16:49:18 -0500

    add fetch speak and rollover to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,25 @@ class Puppy
     toy
   end
 
+  def speak(woof)
+    i=0
+  	while i<woof
+	  	puts "woof"  		
+	  	i += 1
+	 end
+	end
+
+  def rollover
+	  	puts "rolls over"  		
+	end
 end
+
+spot = Puppy.new
+ 
+spot.fetch("ball")
+
+spot.speak(4)
+
+spot.rollover
+
 
