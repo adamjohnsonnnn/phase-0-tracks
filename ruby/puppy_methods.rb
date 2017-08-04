ce84ceff9e85bc49fe910b8f6305bb02ad9e4b78
Adam Johnson <adam.johnson22@gmail.com>
2017-08-04 16:57:26 -0500

    add puppy name

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -16,6 +16,16 @@ class Puppy
   def rollover
 	  	puts "rolls over"  		
 	end
+
+  def age(humanyears)
+  		dogyears = humanyears * 7
+	  	puts "#{dogyears}"  		
+	end
+
+	def namepuppy(fido)
+		puts "Your puppy's name is #{fido}!"
+	end
+
 end
 
 spot = Puppy.new
@@ -25,5 +35,12 @@ spot.fetch("ball")
 spot.speak(4)
 
 spot.rollover
+
+spot.age(4)
+
+puts "What would you like to name your puppy?"
+name = gets.chomp
+
+spot.namepuppy(name)
 
 
