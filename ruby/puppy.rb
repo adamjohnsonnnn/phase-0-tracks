b194dd3b39734e3fbb50a35078411e5f0daadc56
Adam Johnson <adam.johnson22@gmail.com>
2017-08-06 22:55:42 -0500

    add characteristics of puppy species

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Floppiness: very
+Friendly: yes
+Eyes: 2
+Name: varies
+Color: varies
+Fuzziness: High
+Tail: waggy
+Smell: acute
+
+BEHAVIOR -----------------------
+Jump
+Fetch
+Bark
+Lay
