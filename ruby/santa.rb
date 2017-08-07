93f2982f7f6afbd8acb756ca3e8e0e11b4fd7d86
Adam Johnson <adam.johnson22@gmail.com>
2017-08-06 23:09:38 -0500

    create santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+
+	def initialize
+		puts "Santa Claus is back in town!"
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}"
+	end
+
+end
