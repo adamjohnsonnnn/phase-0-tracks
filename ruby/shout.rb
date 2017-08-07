f81a59587cbdb3a82e091cbf6a258b888d64fdba
Adam Johnson <adam.johnson22@gmail.com>
2017-08-07 01:46:07 -0500

    add back in commented module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,17 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + ":("
+#   end
+#   def self.yelling_happily(words)
+#     words + "!!!" + ":)"
+#   end
+# end
+# p Shout.yell_angrily("So frustrating")
+# p Shout.yelling_happily("So awesome")
+
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+
   	def yell_angrily(words)
     	words + "!!!" + " :("
   	end
