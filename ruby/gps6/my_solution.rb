4fbebb82726fe86889da489aa00c6b87b5f11401
Adam Johnson <adam.johnson22@gmail.com>
2017-08-12 13:52:33 -0500

    add all 50 states

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -6,24 +6,33 @@
 # EXPLANATION OF require_relative
 #
 #
-require_relative 'state_data'
+require './state_data.rb'
 
 class VirusPredictor
 
+def self.generate_report(states)
+  states.each do |key, value|
+    new(key, value[:population_density], value[:population]).virus_effects
+  end
+end
+
+#Stantiate instance variables for class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+#Run predicted_deaths and speed_of_soread methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
-  private
+private
 
-  def predicted_deaths(population_density, population, state)
+#Identify how many deaths will occur from this virus based on the population density of each state
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +50,8 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# Determine tha pace with which the virus will spread based on the population density of each state
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,18 +79,25 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+#Driver Code for running all 50 states at once (Remove generate_report Class method to run)
+# STATE_DATA.each do |key, value|
+#   VirusPredictor.new(key, value[:population_density], value[:population]).virus_effects
+# end
+
+VirusPredictor.generate_report(STATE_DATA)
+
 
 
 #=======================================================================
