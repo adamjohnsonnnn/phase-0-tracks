35fd89fa220eedeea8cb1929b10b8d7135de404f
Adam Johnson <adam.johnson22@gmail.com>
2017-08-12 14:28:29 -0500

    simplify speed and predicted deaths

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -36,12 +36,14 @@ private
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    # elsif @population_density >= 150
+    elsif @population_density >= 50 && @population_density < 200
+      number_of_deaths = ((( @population_density / 50 ) / 10 ) * @population).floor 
+    #   number_of_deaths = (@population * 0.3).floor
+    # elsif @population_density >= 100
+    #   number_of_deaths = (@population * 0.2).floor
+    # elsif @population_density >= 50
+    #   number_of_deaths = (@population * 0.1).floor
     else
       number_of_deaths = (@population * 0.05).floor
     end
@@ -55,15 +57,16 @@ private
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
-
     if @population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
+      elsif @population_density >= 50 && @population_density < 200
+      speed = 2.5 - (0.5)*(@population_density.to_i / 50.to_i).to_f
+    # elsif @population_density >= 150
+    #   speed += 1
+    # elsif @population_density >= 100
+    #   speed += 1.5
+    # elsif @population_density >= 50
+    #   speed += 2
     else
       speed += 2.5
     end
