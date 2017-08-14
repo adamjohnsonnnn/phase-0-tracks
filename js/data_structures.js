8b60f9cd886b99cb16c93e4453da4ae908d3eb4f
Adam Johnson <adam.johnson22@gmail.com>
2017-08-14 00:26:32 -0500

    add horse name to array function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,24 @@
 // create color array
-var color = ["blue", "purple", "green", "red"];
+var color_array = ["blue", "purple", "green", "red"];
 
 // create horse array
-var horses = ["Bob", "Frank", "Margie", "Delilah"];
+var horse_array = ["Bob", "Frank", "Margie", "Delilah"];
+
+// add a color to the color array 
+function add_color(color) {
+	color_array.push(color);
+}
+
+// driver code for add color function
+add_color("yellow");
+console.log(color_array);
+
+// add horse name to horse array
+function add_horse(name) {
+	horse_array.push(name);
+}
+
+// driver code to add horse name to horse array
+add_horse("Bonzo");
+console.log(horse_array);
+
