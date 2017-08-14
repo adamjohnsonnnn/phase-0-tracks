7b623676fb1f97572fc51666338bbcb8fea48332
Adam Johnson <adam.johnson22@gmail.com>
2017-08-13 23:59:10 -0500

    reverse word function

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+// start with "hello"
+var input = "hello";
+
+// separate each letter from the word 
+// print the letters in reverse order
+
+// for (var i = hello.length - 1; i >= 0; i-=1) {
+
+function reverse(word){
+	str = ""
+for (var i = word.length - 1; i >= 0; i-=1) {
+	str += word[i] + ""; 
+}
+console.log(str);
+}
+
+reverse(input);
