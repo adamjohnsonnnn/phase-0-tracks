6baa4466c6234ba600c2330eddecb89ae39c7e4c
Adam Johnson <adam.johnson22@gmail.com>
2017-08-15 09:42:32 -0500

    determine and return longest word

--- js/algos.js
+++ js/algos.js
@@ -1,13 +1,17 @@
 word_array = ["to", "thine", "own", "self", "be", "true"]
 
-function longest_word(word_array) {
-  word_length = 0
-  for(var i = 0; i < word_array.length; i++) {
-    if(word_array[i].length > word_length) {
-	word_length = word_array[i].length;
-    }
-  }
-  return word_length;
+function longest_word(array) {
+ // Test code to return the length of the longest word
+ //  for(var i = 0; i < word_array.length; i++) {
+ //    if(word_array[i].length > word_length) {
+	// word_length = word_array[i].length;
+ //    }
+ //  }
+ //  return word_length;
+
+ // Code to return longest word
+ var sort = array.sort((a, b) => b.length - a.length );
+console.log(sort[0]);
 }
 
-console.log(longest_word(word_array));
+longest_word(word_array);
